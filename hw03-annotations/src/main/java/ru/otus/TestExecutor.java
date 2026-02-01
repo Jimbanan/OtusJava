a34b6aa082a169ef22ee58ca8af9fdc1014ac882
Jimbanan <uservice371@mail.ru>
2026-02-01 11:03:02 +0300

    chore: minor fix

--- hw03-annotations/src/main/java/ru/otus/TestExecutor.java
+++ hw03-annotations/src/main/java/ru/otus/TestExecutor.java
@@ -1,5 +1,7 @@
 package ru.otus;
 
+import ru.otus.util.TestRunnerUtils;
+
 public class TestExecutor {
     public static void main(String[] args) {
         TestRunnerUtils.executeTests(TestClass.class);
--- hw03-annotations/src/main/java/ru/otus/util/TestRunnerUtils.java
+++ hw03-annotations/src/main/java/ru/otus/util/TestRunnerUtils.java
@@ -1,4 +1,4 @@
-package ru.otus;
+package ru.otus.util;
 
 import java.lang.annotation.Annotation;
 import java.lang.reflect.InvocationTargetException;
